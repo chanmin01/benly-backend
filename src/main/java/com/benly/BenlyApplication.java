4d63ec0c974a8fa24074115e99f7fc5a0d32ba8b
jeonchanmin <chanmin010702@gmail.com>
2026-07-30 17:20:17 +0900

    fix: JPA Auditing 설정을 별도 config로 분리 (WebMvcTest 컨텍스트 로딩 오류 해결)

--- src/main/java/com/benly/BenlyApplication.java
+++ src/main/java/com/benly/BenlyApplication.java
@@ -1,16 +1,13 @@
 package com.benly;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class BenlyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BenlyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BenlyApplication.class, args);
+    }
 
 }
