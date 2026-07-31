857cc83e547bf968b140d27c73ffc38ba7b27c49
김태기 <>
2026-08-01 00:23:49 +0900

    feat: 메인 질문 중 첫번째 질문 조회

--- src/main/java/com/benly/question/repository/QuestionRepository.java
+++ src/main/java/com/benly/question/repository/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.benly.question.repository;
 
 import com.benly.question.entity.Question;
+import com.benly.session.entity.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findFirstBySessionAndParentIsNullOrderBySeqAsc(Session session);
 }
