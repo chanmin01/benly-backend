60867a7a2030d498d7fb23212db13fcb226885d8
jeonchanmin <chanmin010702@gmail.com>
2026-08-04 17:03:06 +0900

    feat: 히스토리 응답 DTO 및 에러코드 추가

--- src/main/java/com/benly/history/exception/HistoryErrorCode.java
+++ src/main/java/com/benly/history/exception/HistoryErrorCode.java
@@ -1,4 +1,16 @@
 package com.benly.history.exception;
 
-public class HistoryErrorCode {
+import com.benly.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum HistoryErrorCode implements ErrorCode {
+
+    INVALID_COMPANY_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 기업유형입니다.");
+
+    private final HttpStatus status;
+    private final String message;
 }
